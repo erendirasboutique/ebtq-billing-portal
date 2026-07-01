3fab96e778b95667e50e17bb03c3b1ed06478fad
Dylan <hello@erendirasboutique.com>
2026-07-01 04:07:34 -0700

    Import useEffect in AdminLoginPage component

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LanguageToggle from '@/components/LanguageToggle';
 import { getSupabaseBrowser } from '@/lib/supabaseBrowser';
 
