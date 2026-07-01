5d520d038be59dfe017175461b9c917fd5e57a2f
Dylan <hello@erendirasboutique.com>
2026-07-01 03:52:48 -0700

    Replace Google sign-in button with styled version

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -75,10 +75,26 @@ export default function AdminLoginPage() {
               customers.
             </p>
 
-            <button className="button secondary" type="button" onClick={signInWithGoogle}>
-              Continue with Google
-            </button>
+            <button
+  className="button googleButton"
+  type="button"
+  onClick={signInWithGoogle}
+>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 48 48"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <path fill="#FFC107" d="M43.611 20.083H42V20H24v8h11.303C33.654 32.657 29.195 36 24 36c-6.627 0-12-5.373-12-12S17.373 12 24 12c3.059 0 5.842 1.154 7.959 3.041l5.657-5.657C34.053 6.053 29.277 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z"/>
+    <path fill="#FF3D00" d="M6.306 14.691l6.571 4.819C14.655 16.108 18.961 12 24 12c3.059 0 5.842 1.154 7.959 3.041l5.657-5.657C34.053 6.053 29.277 4 24 4c-7.682 0-14.347 4.337-17.694 10.691z"/>
+    <path fill="#4CAF50" d="M24 44c5.176 0 9.86-1.977 13.409-5.192l-6.191-5.238C29.153 35.091 26.685 36 24 36c-5.176 0-9.621-3.326-11.276-7.946l-6.522 5.025C9.512 39.556 16.227 44 24 44z"/>
+    <path fill="#1976D2" d="M43.611 20.083H42V20H24v8h11.303c-.792 2.237-2.223 4.166-4.085 5.57l.003-.002 6.191 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z"/>
+  </svg>
 
+  <span>Continue with Google</span>
+</button>
             <div className="notice" style={{ marginTop: 16 }}>
               Or sign in with email and password.
             </div>
